ed3d3a24b19cd77fb424884c4495b965ccee49df
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-05-24 16:09:37 -0600

    Ascendent and descendent

--- src/components/favorites/Favorites.jsx
+++ src/components/favorites/Favorites.jsx
@@ -1,10 +1,33 @@
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import Card from '../Card/Card.jsx';
 import styles from './Favorites.module.css'
+import { filterCards, orderCards } from '../../Redux/actions.js';
 
 function Favorites({ myFavorites, onClose}) {
+
+   const dispatch = useDispatch();
+   const handleOrder = (event) => {
+      dispatch(orderCards(event.target.value));
+   }
+   const handleFilter = (event) => {
+      dispatch(filterCards(event.target.value));
+   }
+
    return (
       <div>
+         <div>
+            <select name="order" onChange={handleOrder}>
+               <option value="A">Ascendent</option>
+               <option value="D">Descendent</option>
+            </select>
+            <select name="filter" onChange={handleFilter}>
+               <option value="All">All</option>
+               <option value="Male">Male</option>
+               <option value="Female">Female</option>
+               <option value="Genderless">Genderless</option>
+               <option value="unknown">unknown</option>
+            </select>
+         </div>
          <div style={{ display: "flex", justifyContent: "space-between" }}>
             {myFavorites.map(character => (
                <Card
