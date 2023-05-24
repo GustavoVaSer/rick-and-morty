d2af36a1729f90cfd0cdc28b1c49a738994f88b3
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-05-24 15:43:19 -0600

    Reducers

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,7 +1,8 @@
-import { ADD_FAV, REMOVE_FAV } from "./types";
+import { ADD_FAV, FILTER, ORDER, REMOVE_FAV } from "./types";
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [], //que renderizo
+    allCharacters: [] // todos los favoritos
 }
 
 export default function reducer(
@@ -12,15 +13,45 @@ export default function reducer(
         case ADD_FAV:
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, payload]
+                myFavorites: [...state.allCharacters, payload],
+                allCharacters: [...state.allCharacters, payload]
             }
         case REMOVE_FAV:
-            const filteredFavs = state.myFavorites.filter(
+            const filteredFavs = state.allCharacters.filter(
                 fav => fav.id !== Number(payload)
             )
             return {
                 ...state,
-                myFavorites: filteredFavs
+                myFavorites: filteredFavs,
+                allCharacters: filteredFavs
+            }
+        case FILTER:
+            // EXTRA: => Caso "All"
+            if (payload === "All") return {
+                ...state,
+                myFavorites: state.allCharacters
+            }
+            const allCharactersCopy = [...state.allCharacters];
+            const filteredCharacters = allCharactersCopy.filter(
+                character => character.gender === payload);
+            return {
+                ...state,
+                myFavorites: filteredCharacters
+            }
+        case ORDER:
+            let orderedCharacters = [];
+            if (payload === "A") {
+                orderedCharacters = state.allCharacters.sort(
+                    (a, b) => a.id - b.id
+                )
+            } else if (payload === "D") {
+                orderedCharacters = state.allCharacters.sort(
+                    (a, b) => b.id - a.id
+                )
+            }
+            return {
+                ...state,
+                myFavorites: orderedCharacters
             }
             default: 
                 return {...state}
