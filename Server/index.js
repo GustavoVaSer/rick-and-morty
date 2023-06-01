68ff3729fd40876db322a5546eb030498f2043f1
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-01 15:01:38 -0600

    Ruta

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,12 @@
 const http = require("http");
-const characters = require("./utils/data.js")
+const getCharacterById = require("./controllers/getCharacterById.js")
 const PORT = 3001;
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+
     if (req.url.includes("/rickandmorty/character")) {
         const id = req.url.split("/").pop();
-        const character = characters.filter(char => char.id === Number(id))[0];
-        res.writeHead(200, { "content-type": "application/json" });
-        res.end(JSON.stringify(character));
+        getCharacterById(res, id);
     }
 }).listen(PORT, "localhost");
