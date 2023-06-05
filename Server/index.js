2b913d691c9c8fdf45287d3a96c2bcfa993fa336
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-05 14:48:02 -0600

    server express

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,20 @@
-const http = require("http");
-const getCharacterById = require("./controllers/getCharacterById.js")
+const express = require('express');
+const server = express();
 const PORT = 3001;
 
-http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+server.listen(PORT, () => {
+   console.log('Server raised in port: ' + PORT);
+});
 
-    if (req.url.includes("/rickandmorty/character")) {
-        const id = req.url.split("/").pop();
-        getCharacterById(res, id);
-    }
-}).listen(PORT, "localhost");
+// const http = require("http");
+// const getCharacterById = require("./controllers/getCharacterById.js")
+// const PORT = 3001;
+
+// http.createServer((req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+
+//     if (req.url.includes("/rickandmorty/character")) {
+//         const id = req.url.split("/").pop();
+//         getCharacterById(res, id);
+//     }
+// }).listen(PORT, "localhost");
